47c0db7cd43c162d03f9f6791cdb9029ee506ea9
Masato Kurokawa <1>
2021-04-09 15:45:57 +0900

    Level2D最新バージョンの上書き

--- MyGAME/GameTemplate/Game/CaslFile.h
+++ MyGAME/GameTemplate/Game/CaslFile.h
@@ -2,45 +2,71 @@
 #include <list>
 #include <vector>
 
-struct CaslData {
-	//レベルの中の画像の名前
-	std::unique_ptr<char[]> name;
-	//レベルのファイルのパス。
-	std::unique_ptr<char[]> filePath;
-	//DDSのファイルのパス。
-	std::unique_ptr<char[]> ddsFilePath;
-	//座標、拡大率。
-	Vector2 position;
-	Vector2 scale;
-	//横幅、縦幅、レイヤーの優先度
-	int width;
-	int height;
-	int numberLayer = 0;
+/// <summary>
+/// Caslファイルのデータ。
+/// </summary>
+struct CaslData
+{
+public:
+	std::unique_ptr<char[]> name;			//名前。
+	std::unique_ptr<char[]> fileName;		//ファイルのパス。
+	std::unique_ptr<char[]> ddsFileName;	//ddsファイル。
+	std::unique_ptr<char[]> ddsFilePath;	//ddsファイルのパス。
+	Vector2 position;						//座標。
+	int width = 0;							//横幅。
+	int height = 0;							//縦幅。
+	int numberLayer = 0;					//レイヤー優先度。
+	Vector2 scale;							//大きさ。
 };
+
+/// <summary>
+/// caslファイルを読み込む。
+/// </summary>
 class CaslFile
 {
-private:
-	//1文字だけ読み込むのに使う。
-	// , や \n　を読み込むのに使う。
-	void ReadOnlyOneCharacter(FILE* file) const;
-	//int型を読み込むのに使う。
-	int ReadInteger(FILE* file) const;
-	//float型を読み込むのに使う。
-	float ReadDecimal(FILE* file) const;
-
-	std::vector<std::unique_ptr<CaslData>> m_caslDataList;		//Caslデータ。
-
 public:
-	//Caslファイルをロードする。
+	/// <summary>
+	/// caslファイルを読み込む。
+	/// </summary>
+	/// <param name="filePath">ファイルパス。</param>
 	void Load(const char* filePath);
-	//Caslデータの数を返す。後ろのconstはこのメンバ関数はメンバ変数を変更しないという意味。
+	/// <summary>
+	/// Caslデータの数を取得。
+	/// </summary>
+	/// <returns>Caslデータの数。</returns>
 	const int GetNumCaslData() const
 	{
 		return m_caslDataList.size();
 	}
-	//number目のCaslデータを取得する。
-	CaslData* GetCaslData(int number) const {
+	/// <summary>
+	/// Caslデータを取得。
+	/// </summary>
+	/// <param name="number">ナンバー。</param>
+	/// <returns></returns>
+	CaslData* GetCaslData(int number) const
+	{
 		return m_caslDataList[number].get();
 	}
+private:
+	//1文字だけ読み込む、(,)を読み込むのに使う。
+	/// <summary>
+	/// 1文字だけ読み込む。「 , 」や「 \n 」を読み込むのに使う。
+	/// </summary>
+	/// <param name="file">ファイル。</param>
+	void ReadOnlyOneCharacter(FILE* file) const;
+	/// <summary>
+	/// int型を読み込む。
+	/// </summary>
+	/// <param name="file">ファイル。</param>
+	/// <returns>int型の値。</returns>
+	int ReadInteger(FILE* file) const;
+	/// <summary>
+	/// float型を読み込む。
+	/// </summary>
+	/// <param name="file">ファイル。</param>
+	/// <returns>float型の値。</returns>
+	float ReadDecimal(FILE* file) const;
+
+	std::vector<std::unique_ptr<CaslData>> m_caslDataList;		//Caslデータ。
 };
 
--- MyGAME/GameTemplate/Game/MapChip2D.cpp
+++ MyGAME/GameTemplate/Game/MapChip2D.cpp
@@ -5,17 +5,19 @@
 void MapChip2D::Init(CaslData* caslData)
 {
 	SpriteInitData data;
+	//横幅、縦幅。
 	data.m_width = caslData->width;
 	data.m_height = caslData->height;
-
-	data.m_ddsFilePath[0] = caslData->ddsFilePath.get();
+	//画像を読み込む。
+	data.m_ddsFilePath[0] =  caslData->ddsFilePath.get();
 	data.m_fxFilePath = "Assets/shader/sprite.fx";
 	m_sprite.Init(data);
-
+	//座標。
 	m_position.x = caslData->position.x;
 	m_position.y = caslData->position.y;
-
+	//大きさ。
 	m_scale.x = caslData->scale.x;
 	m_scale.y = caslData->scale.y;
-
 }
+
+
