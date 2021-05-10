c4c70aedaf8b76610a209b53e70e2df9b689a4f4
test2021/4/21 <kbc20b11@stu.kawahara.ac.jp>
2021-05-11 01:00:15 +0900

    カウンターのモデルを追加、差し替え

--- MyGAME/GameTemplate/Game/Counter.cpp
+++ MyGAME/GameTemplate/Game/Counter.cpp
@@ -38,7 +38,8 @@ bool Counter::Start()
 
 	m_skinModelRender = NewGO<SkinModelRender>(0);
 
-	m_skinModelRender->Init("Assets/modelData/box2.tkm", nullptr, enModelUpAxisZ, m_position);
+	//m_skinModelRender->Init("Assets/modelData/box2.tkm", nullptr, enModelUpAxisZ, m_position);
+	m_skinModelRender->Init("Assets/modelData/object/counter.tkm", nullptr, enModelUpAxisZ, m_position);
 	m_skinModelRender->InitShader("Assets/shader/model.fx", "VSMain", "VSSkinMain", DXGI_FORMAT_R32G32B32A32_FLOAT);
 
 	return true;
