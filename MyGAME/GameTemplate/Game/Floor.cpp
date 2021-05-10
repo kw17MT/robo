ab3ab72b831217c2bdedb0603e1ff11a40f33c00
Masato Kurokawa <kbc20b20@stu.kawahara.ac.jp>
2021-05-11 08:48:54 +0900

    モデル

---
+++ MyGAME/GameTemplate/Game/Floor.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "Floor.h"
+#include "SkinModelRender.h"
+
+bool Floor::Start()
+{
+	m_skinModelRender = NewGO<SkinModelRender>(0);
+	m_skinModelRender->Init("Assets/modelData/floor/L.tkm", nullptr, enModelUpAxisZ, m_position);
+	m_skinModelRender->InitShader("Assets/shader/model.fx", "VSMain", "VSSkinMain", DXGI_FORMAT_R32G32B32A32_FLOAT);
+
+	return true;
+}
+
+void Floor::Update()
+{
+	m_skinModelRender->SetPosition(m_position);
+}
